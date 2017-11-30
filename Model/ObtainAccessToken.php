25412f9d37981bf644f342dfb3d5a75f4c804ac1
Diego <diego.giglioli@gmail.com>
2017-11-30 15:40:17 -0200

    Adjustes in method payment

--- Model/ObtainAccessToken.php
+++ Model/ObtainAccessToken.php
@@ -49,12 +49,13 @@ class ObtainAccessToken
 
         $apiContext->setConfig(
             array(
-                'mode' => $mode,
-                // 'log.LogEnabled' => true,
-                // 'log.FileName' => '../var/log/paypalplus.log',
-                // 'log.LogLevel' => 'DEBUG', // PLEASE USE `INFO` LEVEL FOR LOGGING IN LIVE ENVIRONMENTS
-                'cache.enabled' => true,
-                'http.CURLOPT_SSLVERSION' => 'CURL_SSLVERSION_TLSv1_2'
+              'http.headers.PayPal-Partner-Attribution-Id' => 'MagentoBrazil_Ecom_PPPlus2',
+              'mode' => $mode,
+              'log.LogEnabled' => true,
+              'log.FileName' => '../var/log/paypalplus.log',
+              'log.LogLevel' => 'DEBUG', // PLEASE USE `INFO` LEVEL FOR LOGGING IN LIVE ENVIRONMENTS
+              'cache.enabled' => true,
+              'http.CURLOPT_SSLVERSION' => 'CURL_SSLVERSION_TLSv1_2'
             )
         );
 
@@ -65,19 +66,11 @@ class ObtainAccessToken
         $item1->setName('My item 1')
             ->setDescription('My description...')
             ->setQuantity('1')
-            ->setPrice('0.50')
-            ->setTax('0.01')
+            ->setPrice('12.00')
+           // ->setTax('0.01')
             ->setsku('asd123')
-            ->setCurrency('USD');
+            ->setCurrency('BRL');
 
-        $item2 = new \PayPal\Api\Item();
-        $item2->setName('My item 2')
-            ->setDescription('My description...')
-            ->setQuantity('2')
-            ->setPrice('0.70')
-            ->setTax('0.01')
-            ->setsku('zxc123')
-            ->setCurrency('USD');
 
         $shippingAddress = new \PayPal\Api\ShippingAddress();
         $shippingAddress->setRecipientName("Enzo Silva")
@@ -89,19 +82,29 @@ class ObtainAccessToken
             ->setPhone("5511987654321")
             ->setState("SP");
 
+        $details = new \PayPal\Api\Details();
+        $details->setShipping(1.00)
+           ->setSubtotal("12.00");
+
         $itemList = new \PayPal\Api\ItemList();
         $itemList->addItem($item1);
-        $itemList->addItem($item2);
         $itemList->setShippingAddress($shippingAddress);
 
         $amount = new \PayPal\Api\Amount();
-        $amount->setCurrency("USD");
-        $amount->setTotal("12");
+        $amount->setCurrency("BRL");
+        $amount->setTotal("13.00");
+        $amount->setDetails($details);
+
+
+        $paymentOptions = new \PayPal\Api\PaymentOptions();
+        $paymentOptions->setAllowedPaymentMethod("IMMEDIATE_PAY");
 
         $transaction = new \PayPal\Api\Transaction();
         $transaction->setDescription("Creating a payment");
         $transaction->setAmount($amount);
-        // $transaction->setItemList($itemList);
+        $transaction->setItemList($itemList);
+        $transaction->setPaymentOptions($paymentOptions);
+
 
         $baseUrl = 'http://paypal.dev';
         $redirectUrls = new \PayPal\Api\RedirectUrls();
@@ -114,8 +117,12 @@ class ObtainAccessToken
         $payment->setTransactions(array($transaction));
 
         try {
+
             $payment->create($apiContext);
-        } catch (\PayPal\Exception\PPConnectionException $ex) {
+        } catch (\PayPal\Exception\PayPalConnectionException $ex) {
+
+            echo $ex->getCode();
+            echo $ex->getData();
             echo "Exception: " . $ex->getMessage() . PHP_EOL;
             $err_data = json_decode($ex->getData(), true);
             print_r($err_data);
@@ -127,88 +134,11 @@ class ObtainAccessToken
         $logger->addWriter($writer);
         $logger->info($payment);
 
-        return  $payment->getApprovalLink();
+        return $payment->getApprovalLink();
+        $retorno =   array('aprovall_url' => $payment->getApprovalLink(),
+                            'mode' =>$mode );
+        return json_encode($retorno);
 
-        $curl = curl_init();
-        $data_string = '{
-                                  "intent": "sale",
-                                  "payer": {
-                                  "payment_method": "paypal"
-                                  },
-                                  "transactions": [
-                                  {
-                                    "amount": {
-                                    "total": "30.11",
-                                    "currency": "USD",
-                                    "details": {
-                                      "subtotal": "30.00",
-                                      "tax": "0.07",
-                                      "shipping": "0.03",
-                                      "handling_fee": "1.00",
-                                      "shipping_discount": "-1.00",
-                                      "insurance": "0.01"
-                                    }
-                                    },
-                                    "description": "The payment transaction description.",
-                                    "custom": "EBAY_EMS_90048630024435",
-                                    "invoice_number": "48787589673",
-                                    "payment_options": {
-                                    "allowed_payment_method": "INSTANT_FUNDING_SOURCE"
-                                    },
-                                    "soft_descriptor": "ECHI5786786",
-                                    "item_list": {
-                                    "items": [
-                                      {
-                                      "name": "hat",
-                                      "description": "Brown hat.",
-                                      "quantity": "5",
-                                      "price": "3",
-                                      "tax": "0.01",
-                                      "sku": "1",
-                                      "currency": "USD"
-                                      },
-                                      {
-                                      "name": "handbag",
-                                      "description": "Black handbag.",
-                                      "quantity": "1",
-                                      "price": "15",
-                                      "tax": "0.02",
-                                      "sku": "product34",
-                                      "currency": "USD"
-                                      }
-                                    ],
-                                    "shipping_address": {
-                                      "recipient_name": "Brian Robinson",
-                                      "line1": "4th Floor",
-                                      "line2": "Unit #34",
-                                      "city": "San Jose",
-                                      "country_code": "US",
-                                      "postal_code": "95131",
-                                      "phone": "011862212345678",
-                                      "state": "CA"
-                                    }
-                                    }
-                                  }
-                                  ],
-                                  "note_to_payer": "Contact us for any questions on your order.",
-                                  "redirect_urls": {
-                                  "return_url": "http://www.paypal.com/return",
-                                  "cancel_url": "http://www.paypal.com/cancel"
-                                  }
-                                }';
 
-          curl_setopt_array($curl, array(
-          CURLOPT_URL => "https://api.sandbox.paypal.com/v1/payments/payment ",
-          CURLOPT_RETURNTRANSFER => true,
-          CURLOPT_CUSTOMREQUEST => "POST",
-          CURLOPT_POSTFIELDS => $data_string,
-          CURLOPT_SSL_VERIFYPEER => false,
-          CURLOPT_HTTPHEADER => array(
-            "authorization: Bearer $cred->accessToken",
-            "content-type: application/json"
-          ),
-        ));
-
-        return $url;
     }
 }
