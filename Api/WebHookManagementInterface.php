d96b0d3bf8e2273b9678d9a5b1e77e5fbe36b774
Diego <diego.giglioli@gmail.com>
2017-12-07 00:50:40 -0200

    Webhooks interface inital code

--- Api/WebHookManagementInterface.php
+++ Api/WebHookManagementInterface.php
@@ -3,10 +3,53 @@ namespace PayPalBR\PayPalPlus\Api;
 
 interface WebHookManagementInterface
 {
-    /**
-     * POST for WebHook api
-     * @param string $param
-     * @return string
-     */
-    public function postWebHook($param);
+        /**
+         * POST for WebHook api
+         * @param string $param
+         * @return string
+         */
+        public function postWebHook($params);
+            /**
+         * GET for WebHook api
+         * @param string $param
+         * @return string
+         */
+        public function getWebHook($id);
+            /**
+         * GET for WebHook api
+         * @param string $id
+         * @return string
+         */
+        public function patchWebHook($params);
+
+        /**
+         * POST for WebHook api
+         * @param string $param
+         * @return string
+         */
+        public function deleteWebHook($id);
+            /**
+         * GET for WebHook api
+         * @param string $id
+         * @return string
+         */
+        public function getEvent($id);
+            /**
+         * POST for WebHook api
+         * @param string $id
+         * @return string
+         */
+        public function verifySignature($params);
+            /**
+         * POST for WebHook api
+         * @param string $param
+         * @return string
+         */
+        public function resendEvent($id);
+        /**
+        * GET for WebHook api
+        * @param string $param
+        * @return string
+        */
+        public function getEventByParams($params);
 }
