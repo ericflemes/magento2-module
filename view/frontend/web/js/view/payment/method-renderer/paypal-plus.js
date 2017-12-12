b2251e53e7bbd4f29271d8e8dab82f7ec3d1239e
Bunyamin <bunyamin@webjump.com.br>
2017-12-12 09:33:34 -0200

    Fetch customer data automatically.

--- view/frontend/web/js/view/payment/method-renderer/paypal-plus.js
+++ view/frontend/web/js/view/payment/method-renderer/paypal-plus.js
@@ -16,7 +16,7 @@ define([
         initializeIframe: function () {
             var self = this;
             var serviceUrl = urlBuilder.build('paypalplus/payment/index');
-            return storage.post(serviceUrl,'')
+            return storage.post(serviceUrl, '')
                 .done(function (response) {
                     var approvalUrl = '';
                     for(var i = 0; i<response.links.length; i++) {
@@ -25,67 +25,67 @@ define([
                         }
                     }
                     console.log("Approval URL: " + approvalUrl);
-                            this.paypalObject = PAYPAL.apps.PPP(
-                            {
-                                "approvalUrl": approvalUrl,
-                                "placeholder": "ppplus",
-                                "mode": "sandbox" ,
-                                "payerFirstName": "Diego",
-                                "payerLastName": " Lisboa",
-                                "payerPhone": "05511998548609",
-                                "payerEmail": "diego.giglioli@gmail.com",
-                                "payerTaxId": "31767933835",
-                                "payerTaxIdType": "BR_CPF",
-                                "language": "pt_BR",
-                                "country": "BR",
-                                "enableContinue": "orderPP",
-                                "disableContinue": "orderPPs",
-                                "iframeHeight": "500",
-                                /**
-                                 * Do stuff after iframe is loaded
-                                 * @returns {undefined}
-                                 */
-                                onLoad: function () {
-                                    console.log("Iframe successfully lo aded !");
-                                },
-                                /**
-                                 * Continue after payment is verifies (continueButton)
-                                 *
-                                 * @param {string} rememberedCards
-                                 * @param {string} payerId
-                                 * @param {string} token
-                                 * @param {string} term
-                                 * @returns {}
-                                 */
-                                onContinue: function () {
-                                    $('#continueButton').hide();
-                                    $('#payNowButton').show();
+                    var customerData = window.checkoutConfig.customerData;
+                    this.paypalObject = PAYPAL.apps.PPP(
+                    {
+                        "approvalUrl": approvalUrl,
+                        "placeholder": "ppplus",
+                        "mode": "sandbox" ,
+                        "payerFirstName": customerData.firstname,
+                        "payerLastName": customerData.lastname,
+                        "payerPhone": "05511998548609",
+                        "payerEmail": customerData.email,
+                        "payerTaxId": customerData.taxvat,
+                        "payerTaxIdType": "BR_CPF",
+                        "language": "pt_BR",
+                        "country": "BR",
+                        "enableContinue": "orderPP",
+                        "disableContinue": "orderPPs",
+                        "iframeHeight": "500",
+                        /**
+                         * Do stuff after iframe is loaded
+                         * @returns {undefined}
+                         */
+                        onLoad: function () {
+                            console.log("Iframe successfully lo aded !");
+                        },
+                        /**
+                         * Continue after payment is verifies (continueButton)
+                         *
+                         * @param {string} rememberedCards
+                         * @param {string} payerId
+                         * @param {string} token
+                         * @param {string} term
+                         * @returns {}
+                         */
+                        onContinue: function () {
+                            $('#continueButton').hide();
+                            $('#payNowButton').show();
 
-                                    var message = {
-                                        message: $.mage.__('Payment has been authorized.')
-                                    };
-                                    self.messageContainer.addSuccessMessage(message);
-                                    $('#ppplus').hide();
+                            var message = {
+                                message: $.mage.__('Payment has been authorized.')
+                            };
+                            self.messageContainer.addSuccessMessage(message);
+                            $('#ppplus').hide();
 
-                                    //end aproved card and payment method, run placePendingOrder
-                                    //self.placePendingOrder();
-                                },
-                                /**
-                                 * Handle iframe error (if payment fails for example)
-                                 *
-                                 * @param {type} err
-                                 * @returns {undefined}
-                                 */
-                                onError: function (err) {
-                                    var message = {
-                                        message: JSON.stringify(err.cause)
-                                    };
-                                    //Display response error
-                                    //that.messageContainer.addErrorMessage(message);
-                                }
-                            });
-
-                    console.log(response);
+                            //end aproved card and payment method, run placePendingOrder
+                            //self.placePendingOrder();
+                        },
+                        /**
+                         * Handle iframe error (if payment fails for example)
+                         *
+                         * @param {type} err
+                         * @returns {undefined}
+                         */
+                        onError: function (err) {
+                            var message = {
+                                message: JSON.stringify(err.cause)
+                            };
+                            //Display response error
+                            //that.messageContainer.addErrorMessage(message);
+                        }
+                    });
+                    // console.log(response);
                 }).fail(function (response) {
                     console.log(response);
                 });
