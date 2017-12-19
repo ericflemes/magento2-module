eadcf72e552bc3a48e3ab2f1a415a467c2cf2b83
Diego <diego.giglioli@gmail.com>
2017-12-19 14:54:56 -0200

    Ajustes

--- Setup/InstallData.php
+++ Setup/InstallData.php
@@ -1,88 +1,72 @@
 <?php
 
-namespace PayPalBR\PayPalPlus\Setup;
+namespace PayPalBR\PayPalPlusMx\Setup;
 
-use Magento\Eav\Setup\EavSetup;
-use Magento\Eav\Setup\EavSetupFactory;
+use Magento\Customer\Setup\CustomerSetupFactory;
+use Magento\Customer\Model\Customer;
+use Magento\Eav\Model\Entity\Attribute\Set as AttributeSet;
+use Magento\Eav\Model\Entity\Attribute\SetFactory as AttributeSetFactory;
 use Magento\Framework\Setup\InstallDataInterface;
 use Magento\Framework\Setup\ModuleContextInterface;
 use Magento\Framework\Setup\ModuleDataSetupInterface;
-use Magento\Eav\Model\Config;
-use Magento\Customer\Model\Customer;
 
+/**
+ * @codeCoverageIgnore
+ */
 class InstallData implements InstallDataInterface
 {
-    private $eavSetupFactory;
-    private $eavConfig;
+    /**
+     * @var CustomerSetupFactory
+     */
+    protected $customerSetupFactory;
 
+    /**
+     * @var AttributeSetFactory
+     */
+    private $attributeSetFactory;
+
+    /**
+     * @param CustomerSetupFactory $customerSetupFactory
+     * @param AttributeSetFactory $attributeSetFactory
+     */
     public function __construct(
-        EavSetupFactory $eavSetupFactory, 
-        Config $eavConfig
+        CustomerSetupFactory $customerSetupFactory,
+        AttributeSetFactory $attributeSetFactory
     ) {
-        $this->setEavSetupFactory($eavSetupFactory);
-        $this->setEavConfig($eavConfig);
+        $this->customerSetupFactory = $customerSetupFactory;
+        $this->attributeSetFactory = $attributeSetFactory;
     }
-
+    /**
+     * {@inheritdoc}
+     */
     public function install(ModuleDataSetupInterface $setup, ModuleContextInterface $context)
     {
-        $eavSetup = $this->getEavSetupFactory()->create(['setup' => $setup]);
-        $eavSetup->addAttribute(
-            Customer::ENTITY,
-            'customer_token',
-            [
-                'label'         => 'Token Access PayPal',
-                'type'          => 'static',
-                'input'         => 'text',
-                'required'      => false,
-                'default'       => '',
 
-            ]
-        );
-        $attribute = $this->getEavConfig()->getAttribute(Customer::ENTITY, 'customer_token');
-        $attribute->setData(
-            'used_in_forms',
-            ['adminhtml_customer_address', 'customer_address_edit', 'customer_register_address']
-        );
+        /** @var CustomerSetup $customerSetup */
+        $customerSetup = $this->customerSetupFactory->create(['setup' => $setup]);
+
+        $customerEntity = $customerSetup->getEavConfig()->getEntityType('customer');
+        $attributeSetId = $customerEntity->getDefaultAttributeSetId();
+
+        /** @var $attributeSet AttributeSet */
+        $attributeSet = $this->attributeSetFactory->create();
+        $attributeGroupId = $attributeSet->getDefaultGroupId($attributeSetId);
+
+        $customerSetup->addAttribute(Customer::ENTITY, 'remembered_card', [
+            'type' => \Magento\Framework\DB\Ddl\Table::TYPE_TEXT,
+            'label' => 'PayPalPlus Remembered Card',
+            'input' => 'text',
+            'backend' => 'PayPalBR\PayPalPlus\Model\Customer\Token'
+        ]);
+
+        $attribute = $customerSetup->getEavConfig()->getAttribute(Customer::ENTITY, 'remembered_card')
+        ->addData([
+            'attribute_set_id' => $attributeSetId,
+            'attribute_group_id' => $attributeGroupId
+        ]);
+
         $attribute->save();
-    }
 
-    /**
-     * @return mixed
-     */
-    public function getEavSetupFactory()
-    {
-        return $this->eavSetupFactory;
-    }
 
-    /**
-     * @param mixed $eavSetupFactory
-     *
-     * @return self
-     */
-    public function setEavSetupFactory($eavSetupFactory)
-    {
-        $this->eavSetupFactory = $eavSetupFactory;
-
-        return $this;
-    }
-
-    /**
-     * @return mixed
-     */
-    public function getEavConfig()
-    {
-        return $this->eavConfig;
-    }
-
-    /**
-     * @param mixed $eavConfig
-     *
-     * @return self
-     */
-    public function setEavConfig($eavConfig)
-    {
-        $this->eavConfig = $eavConfig;
-
-        return $this;
     }
 }
