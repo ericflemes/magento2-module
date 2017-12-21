c552d0c2fdae20be2ffeeaa9efabce6661b8a233
Allan Aievoli <allan@webjump.com.br>
2017-12-21 16:04:29 -0200

    new version 0.2.16

--- view/frontend/web/js/view/payment/method-renderer/paypal-plus.js
+++ view/frontend/web/js/view/payment/method-renderer/paypal-plus.js
@@ -72,11 +72,16 @@ define([
             var telephone = '';
             var customerData = window.checkoutConfig.customerData;
             var mode = window.checkoutConfig.payment.paypalbr_paypalplus.mode === "1" ? 'sandbox' : 'live';
-            if (customerData.addresses.indexOf() == '-1' ) {
+            if (customerData.indexOf() == '-1') {
+                if (customerData.addresses.indexOf() == '-1' ) {
                 telephone = '0000000000';
+                }else{
+                    telephone = customerData.addresses[0].telephone;
+                }
             }else{
                 telephone = customerData.addresses[0].telephone;
             }
+            
 
             this.paypalObject = PAYPAL.apps.PPP(
                 {
