60428428922b0fc17731f9d79ca99386052c6d87
Allan Aievoli <allanaievoli@MacBook-Pro-de-Allan.local>
2018-01-04 14:25:24 -0200

    Adjust paypal phtml show client

--- Block/Payment/Info/PayPalPlus.php
+++ Block/Payment/Info/PayPalPlus.php
@@ -14,4 +14,34 @@ class PayPalPlus extends Info
     {
         $this->setTemplate(self::TEMPLATE);
     }
+
+    public function getTitle()
+    {
+        return $this->getInfo()->getAdditionalInformation('method_title');
+    }
+
+    public function getPayId()
+    {
+        return $this->getInfo()->getAdditionalInformation('pay_id');
+    }
+
+    public function getPayerId()
+    {
+        return $this->getInfo()->getAdditionalInformation('payer_id');
+    }
+
+    public function getToken()
+    {
+        return $this->getInfo()->getAdditionalInformation('token');
+    }
+
+    public function getTerm()
+    {
+        return $this->getInfo()->getAdditionalInformation('term');
+    }
+
+    public function getStatePayPal()
+    {
+        return $this->getInfo()->getAdditionalInformation('state_payPal');
+    }
 }
